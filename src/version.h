6119086880feb4c60c460a51939b1f9175e24cde
qitchain_dev <developer@qitchain.link>
2025-03-12 10:45:12 +0800

    update protocol version to 80022

--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 80021;
+static const int PROTOCOL_VERSION = 80022;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
